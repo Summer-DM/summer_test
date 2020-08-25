94c71de7a07cedd3dbc0d75fb6f24b404eba089c
闫鹏 <yanpeng@apexsoft.com.cn>
2020-08-25 14:33:12 +0800

    提交mybatis逆向工程测试demo

---
+++ src/pojo/User.java
@@ -0,0 +1,93 @@
+package pojo;
+
+public class User {
+    private Integer id;
+
+    private String name;
+
+    private String age;
+
+    private String sex;
+
+    private String addr;
+
+    private String phone;
+
+    private String email;
+
+    private String idCard;
+
+    private String health;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age == null ? null : age.trim();
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr == null ? null : addr.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard == null ? null : idCard.trim();
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health == null ? null : health.trim();
+    }
+}
